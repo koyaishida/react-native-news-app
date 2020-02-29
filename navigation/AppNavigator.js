7b7d078e343651f5bb0dcc1b3f61a779f33b916f
石田航也 <aichibaseball@ishidawatarunari-no-MacBook-Air.local>
2020-02-29 18:06:26 +0900

    Finished news-app

---
+++ navigation/AppNavigator.js
@@ -0,0 +1,56 @@
+import React from "react";
+import {createAppContainer} from "react-navigation";
+import {createStackNavigator} from "react-navigation-stack";
+import {createBottomTabNavigator} from "react-navigation-tabs";
+import HomeScreen from "../screens/HomeScreen";
+import ArticleScreen from "../screens/ArticleScreen"
+import ClipScreen from "../screens/ClipScreen"
+import { FontAwesome } from '@expo/vector-icons';
+
+const HomeStack = createStackNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions:{
+      header: null,
+    }
+  },
+  Article: {
+    //ArticleはonPressの処理で使うkey
+    screen: ArticleScreen,
+  },
+})
+
+const ClipStack = createStackNavigator({
+  Clip: {
+    screen: ClipScreen,
+    navigationOptions:{
+      title: "お気に入り",
+    }
+  },
+  Article: {
+    //ArticleはonPressの処理で使うkey
+    screen: ArticleScreen,
+  },
+})
+
+const AppNavigator = createBottomTabNavigator({
+  Home : {
+    screen : HomeStack,
+    navigationOptions: {
+      tabBarIcon: ({tintColor}) =>{
+        return <FontAwesome name="home" color={tintColor} size={24} />
+      },
+    },
+  },
+
+  Clip : {
+    screen : ClipStack,
+    navigationOptions: {
+      tabBarIcon: ({tintColor}) =>{
+        return <FontAwesome name="bookmark" color={tintColor} size={24} />
+      },
+    },
+  },
+});
+
+export default createAppContainer(AppNavigator)
---
+++ screens/HomeScreen.js
@@ -0,0 +1,59 @@
+import React ,{useState,useEffect}from "react"
+import { StyleSheet, FlatList, SafeAreaView} from "react-native"
+import ListItem from "../components/ListItem"
+import Constants from "expo-constants"
+import axios from "axios"
+import Loading from "../components/Loading"
+
+                        
+
+const styles = StyleSheet.create({
+  container: {
+      flex: 1,
+      backgroundColor: "#fff",
+  },
+  
+})
+
+const URL = `http://newsapi.org/v2/top-headlines?country=jp&apiKey=${Constants.manifest.extra.newsApiKey}`
+
+
+export default HomeScreen = (props) => {
+  const [articles,setArticles] = useState([])
+  const [loading,setLoading] = useState(false)
+
+  useEffect(() => {
+    fetchArticles();
+  }, []);
+
+  const fetchArticles = async() => {
+    setLoading(true);
+    try{
+      const response = await axios.get(URL);
+      setArticles(response.data.articles)
+    }catch(error){
+      console.error(error);
+    }
+    setLoading(false)
+  }
+
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList 
+      data={articles}
+      renderItem={({item})=>(
+        <ListItem
+        imageUrl={item.urlToImage}
+        title={item.title}
+        author={item.author}
+        onPress={()=>props.navigation.navigate("Article",{article:item})}
+                    //ArticleはAppNavigationで指定したkey
+        />
+      )}
+      keyExtractor={(item,index) =>index.toString()}
+      />
+      {loading && <Loading />}
+    </SafeAreaView>   
+    )
+}
